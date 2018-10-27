0876bc8f51828a15efdf960618ba8de4aa15ed98
TheMalson <TRGEHRING@aol.com>
2018-10-27 18:28:12 +0200

    First little Test of Classes & Methods with dummy data

--- app/src/main/java/com/example/franzi/rentrapp/MainActivity.java
+++ app/src/main/java/com/example/franzi/rentrapp/MainActivity.java
@@ -8,11 +8,13 @@ import android.widget.Button;
 import android.widget.RadioButton;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class MainActivity extends AppCompatActivity {
 
     Survey sv;
     SpecificSurvey ss;
-    Question[] questions;
+    Question[] questions = new Question[4];
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,9 +23,11 @@ public class MainActivity extends AppCompatActivity {
 
         //Dummy Daten zum rumspielen
 
-        questions = new Question[2];
-        questions[0].setText("Wie toll findest du unsere App?");
-        questions[1].setText("Hattest du jemals so viel Spaß wie heute?");
+        questions[0] = new Question(1,"Wie toll findest du unsere App?", true,false,false,true,false,false);
+        questions[1] = new Question(1,"Hattest du jemals so viel Spaß wie heute?", true,false,false,true,false,false);
+        questions[2] = new Question(1,"Der erste Durchbruch war der Hammer :-)", true,false,false,true,false,false);
+        questions[3] = new Question(1,"Übel geile APP ihr geilen Leute!!", true,false,false,true,false,false);
+
         boolean[] config = {true,false};
 
         sv = new Survey("LALA", "SAP","S4HANA", "ERP","Neueinführung",questions,config);
@@ -35,30 +39,50 @@ public class MainActivity extends AppCompatActivity {
         //Listener und so
 
         final Button button = findViewById(R.id.saveBtn);
+
         button.setOnClickListener(new View.OnClickListener() {
+
             public void onClick(View v) {
 
-                //Prüfen welcher Button ausgewählt wurde
-                RadioButton[] rb = new RadioButton[5];
-                rb[0] = (RadioButton) findViewById(R.id.radioButton);
-                rb[1] = (RadioButton) findViewById(R.id.radioButton2);
-                rb[2] = (RadioButton) findViewById(R.id.radioButton3);
-                rb[3] = (RadioButton) findViewById(R.id.radioButton4);
-                rb[4] = (RadioButton) findViewById(R.id.radioButton5);
-
-                for(int i=0;i<5;i++){
-                    if(rb[i].isChecked()){
-                        ss.getAnswerArray()[0] = i;
-                    }
-                }
-
+                int currentQuestion = ss.getCurrentQuestionIdx();
                 TextView questionText = (TextView) findViewById(R.id.questionTextView);
-                questionText.setText(questions[1].getText());
 
-                TextView resultText = (TextView) findViewById(R.id.resultTextView);
+                //Prüft ob noch Fragen im Katalog vorhanden sind
+                if (currentQuestion < questions.length-1) {
 
+                    //Prüfen welcher Button ausgewählt wurde & ins Antwortarray eintragen
+                    RadioButton[] rb = new RadioButton[5];
+                    rb[0] = (RadioButton) findViewById(R.id.radioButton);
+                    rb[1] = (RadioButton) findViewById(R.id.radioButton2);
+                    rb[2] = (RadioButton) findViewById(R.id.radioButton3);
+                    rb[3] = (RadioButton) findViewById(R.id.radioButton4);
+                    rb[4] = (RadioButton) findViewById(R.id.radioButton5);
+
+                    for (int i = 0; i < 5; i++) {
+                        if (rb[i].isChecked()) {
+                            ss.getAnswerArray()[currentQuestion] = i + 1;
+                        }
+                    }
+
+                    //Get the next Question
+                    currentQuestion++;
+                    ss.setCurrentQuestionIdx(currentQuestion);
+                    //Check if question left
+                    questionText.setText(questions[currentQuestion].getText());
+
+                    ss.calcResult();
+
+                    TextView resultText = (TextView) findViewById(R.id.resultTextView);
+                    double resultInt = ss.getResultTotal();
+                    String resultSting = Double.toString(resultInt);
+                    resultText.setText(resultSting);
+
+                } else {
+                    questionText.setText("Keine weiteren Fragen mehr");
+                }
             }
         });
+
     }
 
 
