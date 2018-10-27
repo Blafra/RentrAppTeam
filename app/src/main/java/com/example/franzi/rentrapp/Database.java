5308407af35ad5afaa5c3243f6b3a8a44ee73a3e
Blafra <44270174+Blafra@users.noreply.github.com>
2018-10-27 11:50:57 +0200

    Datenbank und Mitarbeiter Klasse angelegt

--- app/src/main/java/com/example/franzi/rentrapp/Database.java
+++ app/src/main/java/com/example/franzi/rentrapp/Database.java
@@ -6,7 +6,7 @@ import java.sql.ResultSet;
 import java.sql.ResultSetMetaData;
 import java.sql.SQLException;
 import java.sql.Statement;
-
+//blala
 public class Database {
 
     private java.sql.Connection con = null;
