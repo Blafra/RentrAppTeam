46495a25091083f2ad0249f60f188bf4cb2bc746
TheMalson <TRGEHRING@aol.com>
2018-10-28 10:12:40 +0100

    Main update zur Start Seite

--- app/src/main/java/com/example/franzi/rentrapp/MainActivity.java
+++ app/src/main/java/com/example/franzi/rentrapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.franzi.rentrapp;
 
+import android.content.Intent;
 import android.provider.MediaStore;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,86 +12,33 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
-    Survey sv;
-    SpecificSurvey ss;
-    Question[] questions = new Question[4];
+    Button btnStart;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_start);
 
-        //Dummy Daten zum rumspielen
-
-        questions[0] = new Question(1,"Wie toll findest du unsere App?", true,false,false,true,false,false);
-        questions[1] = new Question(1,"Hattest du jemals so viel Spaß wie heute?", true,false,false,true,false,false);
-        questions[2] = new Question(1,"Der erste Durchbruch war der Hammer :-)", true,false,false,true,false,false);
-        questions[3] = new Question(1,"Übel geile APP ihr geilen Leute!!", true,false,false,true,false,false);
-
-        boolean[] config = {true,false};
-
-        sv = new Survey("LALA", "SAP","S4HANA", "ERP","Neueinführung",questions,config);
-        ss = new SpecificSurvey(1,questions);
-
-        TextView questionText = (TextView) findViewById(R.id.questionTextView);
-        questionText.setText(questions[0].getText());
-
-        //Listener und so
-
-        final Button button = findViewById(R.id.saveBtn);
-
-        button.setOnClickListener(new View.OnClickListener() {
-
-            public void onClick(View v) {
-
-                int currentQuestion = ss.getCurrentQuestionIdx();
-                TextView questionText = (TextView) findViewById(R.id.questionTextView);
-
-                //Prüft ob noch Fragen im Katalog vorhanden sind
-                if (currentQuestion < questions.length-1) {
-
-                    //Prüfen welcher Button ausgewählt wurde & ins Antwortarray eintragen
-                    RadioButton[] rb = new RadioButton[5];
-                    rb[0] = (RadioButton) findViewById(R.id.radioButton);
-                    rb[1] = (RadioButton) findViewById(R.id.radioButton2);
-                    rb[2] = (RadioButton) findViewById(R.id.radioButton3);
-                    rb[3] = (RadioButton) findViewById(R.id.radioButton4);
-                    rb[4] = (RadioButton) findViewById(R.id.radioButton5);
-
-                    for (int i = 0; i < 5; i++) {
-                        if (rb[i].isChecked()) {
-                            ss.getAnswerArray()[currentQuestion] = i + 1;
-                        }
-                    }
-                    Toast.makeText(getApplicationContext(),"Gespeichert",Toast.LENGTH_LONG).show();
-                    //Get the next Question
-                    currentQuestion++;
-                    ss.setCurrentQuestionIdx(currentQuestion);
-                    //Check if question left
-                    questionText.setText(questions[currentQuestion].getText());
-
-                    ss.calcResult();
-
-
-
-                    TextView resultText = (TextView) findViewById(R.id.resultTextView);
-                    double resultInt = ss.getResultTotal();
-                    String resultSting = Double.toString(resultInt);
-                    resultText.setText(resultSting);
-
-                } else {
-                    questionText.setText("Keine weiteren Fragen mehr");
-                }
-            }
-        });
+        btnStart = (Button) findViewById(R.id.btnStart);
+        /* OnClickListener verwaltet das Klicken auf den Button */
+        btnStart.setOnClickListener(this);
 
     }
 
+    @Override
+    public void onClick(View v) {
+        Intent intent = new Intent(this, Individuell.class);
+        startActivity(intent);
+        this.finish();
+    }
+
+  }
 
 
 
 
 
-}
+
+
