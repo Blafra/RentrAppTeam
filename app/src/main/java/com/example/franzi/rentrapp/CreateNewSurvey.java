4cc6cb287d9e146c40cb3f0d915761695d4ab2cd
Blafra <44270174+Blafra@users.noreply.github.com>
2018-11-19 14:18:48 +0100

    DB Einstellungen

--- app/src/main/java/com/example/franzi/rentrapp/CreateNewSurvey.java
+++ app/src/main/java/com/example/franzi/rentrapp/CreateNewSurvey.java
@@ -1,14 +1,12 @@
 package com.example.franzi.rentrapp;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.Spinner;
 import android.widget.TextView;
-
-import org.w3c.dom.Text;
 
 public class CreateNewSurvey extends AppCompatActivity implements View.OnClickListener {
 
--- app/src/main/java/com/example/franzi/rentrapp/MainActivity.java
+++ app/src/main/java/com/example/franzi/rentrapp/MainActivity.java
@@ -1,17 +1,11 @@
 package com.example.franzi.rentrapp;
 
-import android.content.Context;
 import android.content.Intent;
-import android.provider.MediaStore;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RadioButton;
-import android.widget.TextView;
-import android.widget.Toast;
-
-import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 
